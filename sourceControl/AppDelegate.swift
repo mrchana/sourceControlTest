1c20160181f463c8393b3d7cca983127ff2ab153
Hardeep Chana <hardeep@hardeepchana.com>
2020-05-06 20:31:36 -0400

    Initial Commit

---
+++ sourceControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  sourceControl
+//
+//  Created by Hardeep Chana on 2020-05-06.
+//  Copyright © 2020 Hardeep Chana. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ sourceControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sourceControl
+//
+//  Created by Hardeep Chana on 2020-05-06.
+//  Copyright © 2020 Hardeep Chana. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
